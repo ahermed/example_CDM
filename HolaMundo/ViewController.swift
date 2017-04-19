ba281b94493dba230c07cf2656874c078b741ada
mac-cdm <mac-cdm@mac-cdms-iMac.local>
2017-04-19 17:49:54 -0500

    Initial Commit

---
+++ HolaMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by mac-cdm on 19/04/17.
+//  Copyright © 2017 cdm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
